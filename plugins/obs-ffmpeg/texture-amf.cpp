feb2b39fa0043fd54a61c427faa18eb2921b907d
Nikita Pletnev <nikita.a.pletnev@dxc.com>
2025-03-20 23:48:31 +0100

    obs-ffmpeg: Make AMF encoder utilize all VCNs
    VCNs are HW units which can process encoders in parallel boosting
    performance if several encoders are run simultaneously. Added controller
    to balance load of VCNs.
    
    Using KeyedMutex to synchronize copying from texture from OBS core to
    texture allocated and used in encoder caused submitting a lot of HW fences
    to all HW queues leading to parallel encode queues waiting for other
    queue's job's done and hence significant performance degradation.
    To work this around, encoder's texture is created as shared, it's opened
    and copy is done on a device used in OBS core, using synchronization on CPU.

--- plugins/obs-ffmpeg/texture-amf.cpp
+++ plugins/obs-ffmpeg/texture-amf.cpp
@@ -63,6 +63,11 @@ struct adapter_caps {
 	bool supports_av1 = false;
 };
 
+static const GUID SHARED_TEXTURE_CACHE_GUID = {0xe8cd2e6f,
+					       0x64aa,
+					       0x4df0,
+					       {0xa4, 0x6f, 0x53, 0xd1, 0x6b, 0x8a, 0x9c, 0x19}};
+
 /* ------------------------------------------------------------------------- */
 
 static std::map<uint32_t, adapter_caps> caps;
@@ -79,6 +84,45 @@ enum class amf_codec_type {
 	AVC,
 	HEVC,
 	AV1,
+};
+
+/* Hardware may have several encoding units (also called VCNs) which can process several encoders in parallel.
+ * AMF_VIDEO_ENCODER_CAP_NUM_OF_HW_INSTANCES caps property returns number of VCNs, AMF_VIDEO_ENCODER_INSTANCE_INDEX component property sets VCN id to run on.
+ * vcn_controller is to balance load on VCNs */
+struct vcn_controller {
+private:
+	std::mutex mutex;
+	amf_uint32 vcn_num = 0;
+	std::vector<amf_uint32> vcn_load;
+
+public:
+	vcn_controller() {}
+
+	amf_uint32 get_vcn_num() { return vcn_num; }
+
+	void set_vcn_num(amf_uint32 vcn_num)
+	{
+		std::scoped_lock lock(mutex);
+		if (this->vcn_num == 0 && vcn_num != 0) {
+			this->vcn_num = vcn_num;
+			vcn_load.resize(vcn_num, 0);
+		}
+	}
+
+	amf_uint32 get_next_vcn_id()
+	{
+		std::scoped_lock lock(mutex);
+		auto it = std::min_element(vcn_load.begin(), vcn_load.end());
+		amf_uint32 id = (amf_uint32)std::distance(vcn_load.begin(), it);
+		vcn_load[id]++;
+		return id;
+	}
+
+	void enc_ends(amf_uint32 vcn_id)
+	{
+		std::scoped_lock lock(mutex);
+		vcn_load[vcn_id]--;
+	}
 };
 
 struct amf_base {
@@ -126,9 +170,18 @@ struct amf_base {
 		enc_instance_counter++;
 	}
 
-	virtual ~amf_base() = default;
+	virtual ~amf_base() { vcn_ctrl[codec].enc_ends(hw_enc_id); }
 
-	virtual void init() {}
+	virtual void init()
+	{
+		vcn_ctrl.try_emplace(amf_codec_type::AVC);
+		vcn_ctrl.try_emplace(amf_codec_type::HEVC);
+		vcn_ctrl.try_emplace(amf_codec_type::AV1);
+	}
+
+	virtual amf_uint32 get_vcn_num() { return vcn_ctrl[codec].get_vcn_num(); }
+	virtual void set_vcn_num(amf_uint32 vcn_num) { vcn_ctrl[codec].set_vcn_num(vcn_num); }
+	virtual amf_uint32 get_next_vcn_id() { return vcn_ctrl[codec].get_next_vcn_id(); }
 
 protected:
 	virtual void terminate()
@@ -148,11 +201,16 @@ protected:
 			amf_context = nullptr;
 		}
 	}
+
+private:
+	// Different codecs may have different number of VCNs, so separate controllers
+	static std::map<amf_codec_type, vcn_controller> vcn_ctrl;
 };
 
 std::mutex amf_base::context_mutex;
 AMFContextPtr amf_base::amf_context;
 unsigned amf_base::enc_instance_counter = 0;
+std::map<amf_codec_type, vcn_controller> amf_base::vcn_ctrl;
 
 using d3dtex_t = ComPtr<ID3D11Texture2D>;
 using buf_t = std::vector<uint8_t>;
@@ -165,6 +223,8 @@ struct amf_texencode : amf_base, public AMFSurfaceObserver {
 	std::mutex textures_mutex;
 	std::vector<d3dtex_t> available_textures;
 	std::unordered_map<AMFSurface *, d3dtex_t> active_textures;
+
+	ComPtr<ID3D11Query> sync_query;
 
 	static ComPtr<ID3D11Device> device;
 	static ComPtr<ID3D11DeviceContext> context;
@@ -392,11 +452,13 @@ static void add_output_tex(amf_texencode *enc, ComPtr<ID3D11Texture2D> &output_t
 	D3D11_TEXTURE2D_DESC desc;
 	from->GetDesc(&desc);
 	desc.BindFlags = D3D11_BIND_RENDER_TARGET | D3D11_BIND_SHADER_RESOURCE;
-	desc.MiscFlags = 0;
+	desc.MiscFlags = D3D11_RESOURCE_MISC_SHARED;
 
 	hr = device->CreateTexture2D(&desc, nullptr, &output_tex);
 	if (FAILED(hr))
 		throw HRError("Failed to create texture", hr);
+
+	output_tex->SetEvictionPriority(DXGI_RESOURCE_PRIORITY_MAXIMUM);
 }
 
 static inline bool get_available_tex(amf_texencode *enc, ComPtr<ID3D11Texture2D> &output_tex)
@@ -831,6 +893,159 @@ try {
 	/* ------------------------------------ */
 	/* do actual encode                     */
 
+	amf_encode_base(enc, amf_surf, packet, received_packet);
+	return true;
+
+} catch (const char *err) {
+	amf_texencode *enc = (amf_texencode *)data;
+	error("%s: %s", __FUNCTION__, err);
+	return false;
+
+} catch (const amf_error &err) {
+	amf_texencode *enc = (amf_texencode *)data;
+	error("%s: %s: %ls", __FUNCTION__, err.str, amf_trace->GetResultText(err.res));
+	*received_packet = false;
+	return false;
+
+} catch (const HRError &err) {
+	amf_texencode *enc = (amf_texencode *)data;
+	error("%s: %s: 0x%lX", __FUNCTION__, err.str, err.hr);
+	*received_packet = false;
+	return false;
+}
+
+static void copy_on_obs_device(amf_texencode *enc, ID3D11Device *obs_device, ID3D11Texture2D *tex_input,
+			       ID3D11Texture2D *tex_output, uint64_t lock_key, uint64_t next_key)
+{
+	/* This function is made to avoid using KeyedMutex and Copy on encoder's d3d device
+	 * because doing so causes excessive sync objects put to encoder's gpu queues
+	 * causing performance degradation when more than one encoder hw instance is used.
+	 * The copy is done on d3d device used by OBS, where input tex was created.
+	 * For this, output texture is created as shared and opened on OBS device through shared handle.
+	 * For output texture on encoder's device CPU synchronization is used via ID3D11Query */
+
+	HANDLE handle;
+	HRESULT hr;
+	ComPtr<ID3D11Texture2D> tex_output_obs;
+	ComPtr<IDXGIResource> resource;
+	ComPtr<ID3D11DeviceContext> context;
+	obs_device->GetImmediateContext(&context);
+
+	UINT data_size = sizeof(ID3D11Texture2D *);
+	ComPtr<IUnknown> tmp;
+	hr = tex_output->GetPrivateData(SHARED_TEXTURE_CACHE_GUID, &data_size, (void *)&tmp);
+
+	if (tmp) {
+		tex_output_obs = ComQIPtr<ID3D11Texture2D>(tmp);
+	} else {
+		hr = tex_output->QueryInterface(__uuidof(IDXGIResource), (void **)&resource);
+		if (FAILED(hr))
+			throw HRError("QueryInterface(IDXGIResource) failed", hr);
+
+		hr = resource->GetSharedHandle(&handle);
+		if (FAILED(hr))
+			throw HRError("GetSharedHandle failed", hr);
+
+		hr = obs_device->OpenSharedResource((HANDLE)(uintptr_t)handle, __uuidof(ID3D11Resource),
+						    (void **)&tex_output_obs);
+		if (FAILED(hr))
+			throw HRError("OpenSharedResource failed", hr);
+
+		tex_output_obs->SetEvictionPriority(DXGI_RESOURCE_PRIORITY_MAXIMUM);
+
+		hr = tex_output->SetPrivateDataInterface(SHARED_TEXTURE_CACHE_GUID, tex_output_obs.Get());
+	}
+
+	ComPtr<IDXGIKeyedMutex> km;
+	hr = tex_input->QueryInterface(__uuidof(IDXGIKeyedMutex), (void **)&km);
+	if (FAILED(hr))
+		throw HRError("QueryInterface(IDXGIKeyedMutex) failed", hr);
+
+	km->AcquireSync(lock_key, INFINITE);
+	context->CopyResource((ID3D11Resource *)tex_output_obs, (ID3D11Resource *)tex_input);
+	context->Flush();
+	km->ReleaseSync(next_key);
+
+	if (enc->sync_query == nullptr) {
+		D3D11_QUERY_DESC query_desc;
+		query_desc.Query = D3D11_QUERY_EVENT;
+		query_desc.MiscFlags = 0;
+		hr = obs_device->CreateQuery(&query_desc, &enc->sync_query);
+		if (FAILED(hr))
+			throw HRError("CreateQuery failed", hr);
+	}
+
+	// Wait until copy is done and output texture is ready to go into encoder
+
+	context->End(enc->sync_query);
+
+	BOOL data = FALSE;
+	int count = 0;
+	HRESULT result = context->GetData(enc->sync_query, &data, sizeof(BOOL), 0);
+
+	while (((S_FALSE == result) || (data == FALSE)) && (DXGI_ERROR_DEVICE_REMOVED != result)) {
+		count++;
+		if ((count % 100000) == 0) {
+			os_sleep_ms(1); // need to reduce CPU load –
+		}
+
+		result = context->GetData(enc->sync_query, &data, sizeof(BOOL), 0);
+	}
+}
+
+static bool amf_encode_tex2(void *data, encoder_texture *texture, int64_t pts, uint64_t lock_key, uint64_t *next_key,
+			    encoder_packet *packet, bool *received_packet)
+try {
+	amf_texencode *enc = (amf_texencode *)data;
+	ComPtr<ID3D11Texture2D> output_tex;
+	ComPtr<ID3D11Texture2D> input_tex;
+	ComPtr<IDXGIKeyedMutex> km;
+	AMFSurfacePtr amf_surf;
+	AMF_RESULT res;
+
+	if (texture->handle == GS_INVALID_HANDLE) {
+		*next_key = lock_key;
+		throw "Encode failed: bad texture handle";
+	}
+
+	/* ------------------------------------ */
+	/* get the input tex and device used to create it */
+
+	obs_enter_graphics();
+	ComPtr<ID3D11Device> obs_device = (ID3D11Device *)gs_get_device_obj();
+	input_tex = (ID3D11Texture2D *)gs_texture_get_obj(texture->tex[0]);
+
+	/* ------------------------------------ */
+	/* get an output tex                    */
+
+	get_output_tex(enc, output_tex, input_tex);
+
+	/* ------------------------------------ */
+	/* copy to output tex                   */
+
+	copy_on_obs_device(enc, obs_device, input_tex, output_tex, lock_key, *next_key);
+	obs_leave_graphics();
+
+	/* ------------------------------------ */
+	/* map output tex to amf surface        */
+
+	res = enc->amf_context->CreateSurfaceFromDX11Native(output_tex, &amf_surf, enc);
+	if (res != AMF_OK)
+		throw amf_error("CreateSurfaceFromDX11Native failed", res);
+
+	int64_t last_ts = convert_to_amf_ts(enc, pts - 1);
+	int64_t cur_ts = convert_to_amf_ts(enc, pts);
+
+	amf_surf->SetPts(cur_ts);
+	amf_surf->SetProperty(L"PTS", pts);
+
+	{
+		std::scoped_lock lock(enc->textures_mutex);
+		enc->active_textures[amf_surf.GetPtr()] = output_tex;
+	}
+
+	/* ------------------------------------ */
+	/* do actual encode                     */
 	amf_encode_base(enc, amf_surf, packet, received_packet);
 	return true;
 
@@ -1473,6 +1688,12 @@ static void amf_avc_create_internal(amf_base *enc, obs_data_t *settings)
 		caps->GetProperty(AMF_VIDEO_ENCODER_CAP_MAX_THROUGHPUT, &enc->max_throughput);
 		caps->GetProperty(AMF_VIDEO_ENCODER_CAP_REQUESTED_THROUGHPUT, &enc->requested_throughput);
 		caps->GetProperty(AMF_VIDEO_ENCODER_CAP_ROI, &enc->roi_supported);
+
+		if (enc->get_vcn_num() == 0) {
+			amf_uint32 vcn_num;
+			caps->GetProperty(AMF_VIDEO_ENCODER_CAP_NUM_OF_HW_INSTANCES, &vcn_num);
+			enc->set_vcn_num(vcn_num);
+		}
 	}
 
 	const char *preset = obs_data_get_string(settings, "preset");
@@ -1489,6 +1710,7 @@ static void amf_avc_create_internal(amf_base *enc, obs_data_t *settings)
 	set_avc_property(enc, OUTPUT_COLOR_PRIMARIES, enc->amf_primaries);
 	set_avc_property(enc, FULL_RANGE_COLOR, enc->full_range);
 	set_avc_property(enc, FRAMERATE, enc->amf_frame_rate);
+	set_avc_property(enc, INSTANCE_INDEX, enc->hw_enc_id = enc->get_next_vcn_id());
 
 	amf_avc_init(enc, settings);
 
@@ -1593,7 +1815,7 @@ static void register_avc()
 	amf_encoder_info.create = amf_avc_create_texencode;
 	amf_encoder_info.destroy = amf_destroy;
 	amf_encoder_info.update = amf_avc_update;
-	amf_encoder_info.encode_texture = amf_encode_tex;
+	amf_encoder_info.encode_texture2 = amf_encode_tex2;
 	amf_encoder_info.get_defaults = amf_defaults;
 	amf_encoder_info.get_properties = amf_avc_properties;
 	amf_encoder_info.get_extra_data = amf_extra_data;
@@ -1794,6 +2016,12 @@ static void amf_hevc_create_internal(amf_base *enc, obs_data_t *settings)
 		caps->GetProperty(AMF_VIDEO_ENCODER_HEVC_CAP_MAX_THROUGHPUT, &enc->max_throughput);
 		caps->GetProperty(AMF_VIDEO_ENCODER_HEVC_CAP_REQUESTED_THROUGHPUT, &enc->requested_throughput);
 		caps->GetProperty(AMF_VIDEO_ENCODER_HEVC_CAP_ROI, &enc->roi_supported);
+
+		if (enc->get_vcn_num() == 0) {
+			amf_uint32 vcn_num;
+			caps->GetProperty(AMF_VIDEO_ENCODER_HEVC_CAP_NUM_OF_HW_INSTANCES, &vcn_num);
+			enc->set_vcn_num(vcn_num);
+		}
 	}
 
 	const bool is10bit = enc->amf_format == AMF_SURFACE_P010;
@@ -1814,6 +2042,7 @@ static void amf_hevc_create_internal(amf_base *enc, obs_data_t *settings)
 	set_hevc_property(enc, OUTPUT_COLOR_PRIMARIES, enc->amf_primaries);
 	set_hevc_property(enc, NOMINAL_RANGE, enc->full_range);
 	set_hevc_property(enc, FRAMERATE, enc->amf_frame_rate);
+	set_hevc_property(enc, INSTANCE_INDEX, enc->hw_enc_id = enc->get_next_vcn_id());
 
 	if (is_hdr) {
 		const int hdr_nominal_peak_level = pq ? (int)obs_get_video_hdr_nominal_peak_level() : (hlg ? 1000 : 0);
@@ -1925,7 +2154,7 @@ static void register_hevc()
 	amf_encoder_info.create = amf_hevc_create_texencode;
 	amf_encoder_info.destroy = amf_destroy;
 	amf_encoder_info.update = amf_hevc_update;
-	amf_encoder_info.encode_texture = amf_encode_tex;
+	amf_encoder_info.encode_texture2 = amf_encode_tex2;
 	amf_encoder_info.get_defaults = amf_defaults;
 	amf_encoder_info.get_properties = amf_hevc_properties;
 	amf_encoder_info.get_extra_data = amf_extra_data;
@@ -2116,6 +2345,12 @@ static void amf_av1_create_internal(amf_base *enc, obs_data_t *settings)
 		caps->GetProperty(AMF_VIDEO_ENCODER_AV1_CAP_REQUESTED_THROUGHPUT, &enc->requested_throughput);
 		/* For some reason there's no specific CAP for AV1, but should always be supported */
 		enc->roi_supported = true;
+
+		if (enc->get_vcn_num() == 0) {
+			amf_uint32 vcn_num;
+			caps->GetProperty(AMF_VIDEO_ENCODER_AV1_CAP_NUM_OF_HW_INSTANCES, &vcn_num);
+			enc->set_vcn_num(vcn_num);
+		}
 	}
 
 	const bool is10bit = enc->amf_format == AMF_SURFACE_P010;
@@ -2133,6 +2368,7 @@ static void amf_av1_create_internal(amf_base *enc, obs_data_t *settings)
 	set_av1_property(enc, OUTPUT_TRANSFER_CHARACTERISTIC, enc->amf_characteristic);
 	set_av1_property(enc, OUTPUT_COLOR_PRIMARIES, enc->amf_primaries);
 	set_av1_property(enc, FRAMERATE, enc->amf_frame_rate);
+	set_av1_property(enc, ENCODER_INSTANCE_INDEX, enc->hw_enc_id = enc->get_next_vcn_id());
 
 	amf_av1_init(enc, settings);
 
@@ -2234,7 +2470,7 @@ static void register_av1()
 	amf_encoder_info.create = amf_av1_create_texencode;
 	amf_encoder_info.destroy = amf_destroy;
 	amf_encoder_info.update = amf_av1_update;
-	amf_encoder_info.encode_texture = amf_encode_tex;
+	amf_encoder_info.encode_texture2 = amf_encode_tex2;
 	amf_encoder_info.get_defaults = amf_av1_defaults;
 	amf_encoder_info.get_properties = amf_av1_properties;
 	amf_encoder_info.get_extra_data = amf_extra_data;
